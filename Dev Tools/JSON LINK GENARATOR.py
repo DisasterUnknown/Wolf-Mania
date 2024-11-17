5f511a4946d69eebb7c987375e95f8ea238c2f76
DisasterUnknown <priyashanthafdo26@gmail.com>
2024-11-17 18:11:14 +0530

    added infinite mage

---
+++ Dev Tools/JSON LINK GENARATOR.py
@@ -0,0 +1,4 @@
+link = ""
+
+for i in range(15):
+    link = f"{link},\n"
--- JSON/Solo Max-Level Newbie/Chapter creator.py
+++
@@ -1,5 +0,0 @@
-numberOfChapters = int(input("Enter the number of chapters: "))
-
-for i in range(1, numberOfChapters + 1):
-    with open(f'Chapter {i}.json', 'w') as file:
-        pass
